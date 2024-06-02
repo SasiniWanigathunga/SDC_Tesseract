299005aec02c6f9661277d7326a81ba01678cb58
DidulaThavisha <86177477+DidulaThavisha@users.noreply.github.com>
2024-06-02 20:36:02 +0530

    daily

--- Assets/Game_Assests/Script/CreditCount_.cs
+++ Assets/Game_Assests/Script/CreditCount_.cs
@@ -13,6 +13,7 @@ public class CreditCount_ : MonoBehaviour
     public int creditCount = 0;
     public TMP_Text creditText;
     private string powerConsumptionUrl = "http://20.15.114.131:8080/api/power-consumption/current/view";
+    private string daily_consumption_url = "http://20.15.114.131:8080/api/power-consumption/current-month/daily/view";
     private string yearly_consumption_url = "http://20.15.114.131:8080/api/power-consumption/all/view";
     int cumulativeReward = 0;
 
@@ -85,76 +86,102 @@ public class CreditCount_ : MonoBehaviour
             int rewardFromPast = (int)(1000000 / (1000 + pastConsumption));
             Debug.Log("Reward from past: " + rewardFromPast);
 
-
-            using (UnityWebRequest request = UnityWebRequest.Get(powerConsumptionUrl))
+            using (UnityWebRequest daily_request = UnityWebRequest.Get(daily_consumption_url))
             {
-                request.SetRequestHeader("accept", "*/*");
-                //string jwtToken = GlobalManager_.Instance.JwtToken;
-                Debug.Log("JWT Token: " + jwtToken);
-                //string authHeaderValue = "Bearer " + jwtToken;
-                Debug.Log("Authorization: " + authHeaderValue);
-                request.SetRequestHeader("Authorization", authHeaderValue);
-                yield return request.SendWebRequest();
-
-                if (request.result != UnityWebRequest.Result.Success)
+                daily_request.SetRequestHeader("accept", "*/*");
+                daily_request.SetRequestHeader("Authorization", authHeaderValue);
+                yield return daily_request.SendWebRequest();
+                if (daily_request.result != UnityWebRequest.Result.Success)
                 {
-                    Debug.Log("Error: " + request.error);
+                    Debug.Log("Daily_Consumption_API_is"+daily_request.error);
                 }
                 else
-                {   
-                    Debug.Log("Current consumption: " + request.downloadHandler.text);
+                {
+                    string dailyJsonText = daily_request.downloadHandler.text;
+                    Debug.Log("Daily consumption: " + dailyJsonText);
+                    Dictionary<string, DailyPowerConsumptionView> dailyPowerConsumptionDict = JsonConvert.DeserializeObject<Dictionary<string, DailyPowerConsumptionView>>(dailyJsonText);
 
+                    // Extract dailyUnits data
+                    Dictionary<string, float> dailyUnits = dailyPowerConsumptionDict["dailyPowerConsumptionView"].dailyUnits ?? new Dictionary<string, float>();
 
-                    string consumptionJson = request.downloadHandler.text;
-                    Dictionary<string, object> consumptionData = JsonConvert.DeserializeObject<Dictionary<string, object>>(consumptionJson);
-                    float currentConsumption_ =  Convert.ToSingle(consumptionData["currentConsumption"]);
-
-                    int currentConsumption = (int)currentConsumption_;
-                    int rewardFromCurrentDay = 100000 / (1000 + currentConsumption);
-                    Debug.Log("Reward from current: " + rewardFromCurrentDay);
-
-                    int varReward = 0;
-
-                    for (int c = 0; c <= 6; c++)
+                    int counter = 1;
+                    int accumulatedDailyConsumption = 0;
+                    foreach (var dailyUnit in dailyUnits)
                     {
-                        
-                        using (UnityWebRequest realtimeRequest = UnityWebRequest.Get(powerConsumptionUrl))
-                        {
-                            realtimeRequest.SetRequestHeader("accept", "*/*");
-                            realtimeRequest.SetRequestHeader("Authorization", authHeaderValue);
-                            yield return realtimeRequest.SendWebRequest();
-
-                            if (realtimeRequest.result != UnityWebRequest.Result.Success)
-                            {
-                                Debug.Log("Error: " + realtimeRequest.error);
-                            }
-                            else
-                            {
-                                string consumptionJsonTemp = realtimeRequest.downloadHandler.text;
-                                Dictionary<string, object> consumptionDataTeamp = JsonConvert.DeserializeObject<Dictionary<string, object>>(consumptionJsonTemp);
-                                float currentConsumptionTemp =  Convert.ToSingle(consumptionDataTeamp["currentConsumption"]);
-                                int realtimeConsumption = (int)currentConsumptionTemp;
-                                int consumptionDifference = realtimeConsumption - currentConsumption;
-                                currentConsumption = realtimeConsumption;
-                                Debug.Log("Consumption difference: " + consumptionDifference);
-
-                                if (consumptionDifference < 0.5)
-                                {
-                                    varReward = varReward + 100;
-                                }
-
-                                cumulativeReward = rewardFromPast + rewardFromCurrentDay + varReward;
-                                Debug.Log("Cumulative reward: " + cumulativeReward);          
-                                
-                            }
+                        if (counter >= day){
+                            break;
                         }
-                        yield return new WaitForSeconds(10);
-                        
-
+                        accumulatedDailyConsumption += (int)dailyUnit.Value;
+                        counter++;
                     }
-                    
+                    //DailyPowerConsumptionView dailyPowerConsumptionView_ = dailyPowerConsumptionViewRoot.dailyPowerConsumptionViews;
+                    //Debug.Log("Daily consumption view: " + dailyPowerConsumptionView_.year);
+                    //Dictionary<string, float> dailyUnits = dailyPowerConsumptionView.dailyUnits;
+                    // Debug.Log("Daily units: " + dailyUnits);
+                                
                 }
-            } 
+                accumulatedDailyConsumption = (int)(accumulatedDailyConsumption / day);
+
+                using (UnityWebRequest request = UnityWebRequest.Get(powerConsumptionUrl))
+                {
+                    request.SetRequestHeader("accept", "*/*");
+                    request.SetRequestHeader("Authorization", authHeaderValue);
+                    yield return request.SendWebRequest();
+
+                    if (request.result != UnityWebRequest.Result.Success)
+                    {
+                        Debug.Log("Error: " + request.error);
+                    }
+                    else
+                    {   
+                        string consumptionJson = request.downloadHandler.text;
+                        Dictionary<string, object> consumptionData = JsonConvert.DeserializeObject<Dictionary<string, object>>(consumptionJson);
+                        float currentConsumption_ =  Convert.ToSingle(consumptionData["currentConsumption"]);
+                        int currentConsumption = (int)currentConsumption_;
+                        int rewardFromCurrentDay = 100000 / (1000 + currentConsumption);
+                        Debug.Log("Reward from current: " + rewardFromCurrentDay);
+
+                        int varReward = 0;
+                        for (int c = 0; c <= 6; c++)
+                        {                            
+                            using (UnityWebRequest realtimeRequest = UnityWebRequest.Get(powerConsumptionUrl))
+                            {
+                                realtimeRequest.SetRequestHeader("accept", "*/*");
+                                realtimeRequest.SetRequestHeader("Authorization", authHeaderValue);
+                                yield return realtimeRequest.SendWebRequest();
+
+                                if (realtimeRequest.result != UnityWebRequest.Result.Success)
+                                {
+                                    Debug.Log("Error: " + realtimeRequest.error);
+                                }
+                                else
+                                {
+                                    string consumptionJsonTemp = realtimeRequest.downloadHandler.text;
+                                    Dictionary<string, object> consumptionDataTeamp = JsonConvert.DeserializeObject<Dictionary<string, object>>(consumptionJsonTemp);
+                                    float currentConsumptionTemp =  Convert.ToSingle(consumptionDataTeamp["currentConsumption"]);
+                                    int realtimeConsumption = (int)currentConsumptionTemp;
+                                    int consumptionDifference = realtimeConsumption - currentConsumption;
+                                    currentConsumption = realtimeConsumption;
+                                    Debug.Log("Consumption difference: " + consumptionDifference);
+
+                                    if (consumptionDifference < 0.5)
+                                    {
+                                        varReward = varReward + 100;
+                                    }
+
+                                    cumulativeReward = rewardFromPast + rewardFromCurrentDay + varReward;
+                                    Debug.Log("Cumulative reward: " + cumulativeReward);          
+                                    
+                                }
+                            }
+                            yield return new WaitForSeconds(10);
+                            
+
+                        }
+                        
+                    }
+                } 
+            }      
         }
     }
 
@@ -173,6 +200,20 @@ public class CreditCount_ : MonoBehaviour
     {
         public List<YearlyPowerConsumptionView> yearlyPowerConsumptionViews { get; set; }
     }
+
+    public class DailyPowerConsumptionView
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public Dictionary<string, float> dailyUnits { get; set; }
+    }
+    public class RootDaily
+    {
+        public Dictionary<string, DailyPowerConsumptionView> dailyPowerConsumptionViews { get; set; }
+        
+    }
+
+
 
 //     IEnumerator GetPastConsumption()
 //     {
